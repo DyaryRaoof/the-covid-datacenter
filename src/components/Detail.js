3d12138bddaac84416c908d5eeb33e5528455137
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-15 19:51:48 +0300

    Add linter files

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,5 @@
-const Home = () => {
+const Detail = () => {
   return <div>Hello from Detail</div>;
 };
 
-export default Home;
+export default Detail;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div>Hello from Header</div>;
+};
+
+export default Header;
