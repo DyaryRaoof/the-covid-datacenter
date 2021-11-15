1971d8153e50fab92e88259b5b355d423376602e
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-15 19:58:38 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Detail from './components/Detail';
+
 function App() {
-  return <div />;
+  return (
+    <main>
+      <Header />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/detail" element={<Detail />} />
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
 export default App;
