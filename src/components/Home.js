428710b5a3ef39dcc41565032aeebb75e1ce0f80
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-15 22:49:30 +0300

    Add Home page structure and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,72 @@
-const Home = () => <div>Hello from home</div>;
+import { Row, Col } from 'react-bootstrap';
+import './Home.css';
+import { BsArrowRightCircle } from 'react-icons/bs';
+import europe from '../images/europe-map.png';
+
+const countries = [
+  { name: 'CZECH REPUBLIC', number: 119, id: 1 },
+  { name: 'CZECH REPUBLIC', number: 119, id: 2 },
+  { name: 'CZECH REPUBLIC', number: 119, id: 3 },
+  { name: 'CZECH REPUBLIC', number: 119, id: 4 },
+  { name: 'CZECH REPUBLIC', number: 119, id: 5 },
+  { name: 'CZECH REPUBLIC', number: 119, id: 6 },
+  { name: 'CZECH REPUBLIC', number: 119, id: 7 },
+  { name: 'CZECH REPUBLIC', number: 119, id: 8 },
+];
+const Home = () => {
+  let alternate = true;
+  let nextLightColor = 0;
+  const alternateColors = (index) => {
+    if (index === nextLightColor && alternate) {
+      alternate = false;
+      nextLightColor = index + 3;
+      return 'light-background';
+    }
+    if (index === nextLightColor && alternate === false) {
+      alternate = true;
+      nextLightColor = index + 1;
+      return 'light-background';
+    }
+
+    return '';
+  };
+  return (
+    <main>
+      <section className="top-section">
+        <Row>
+          <Col>
+            <img className="w-100 opacity-25 ms-4" src={europe} alt="Europe map" />
+          </Col>
+          <Col>
+            <div className=" top-section-content d-flex flex-column justify-content-center align-items-start">
+              <h1>Europe</h1>
+              <span>6,958</span>
+            </div>
+          </Col>
+        </Row>
+      </section>
+      <span className="stats">STATS BY COUNTRY</span>
+      <section>
+        <div className="country-grid">
+          {countries.map((country, index) => (
+            <div className={alternateColors(index)} key={country.id}>
+              <div className="d-flex justify-content-end me-2 mt-2">
+                <BsArrowRightCircle />
+              </div>
+              <div className="d-flex flex-column align-items-end justify-content-end text-end my-3 me-2 single-grid-item">
+                <h1>
+                  CZECH
+                  <br />
+                  REPUBLIC
+                </h1>
+                <span>965</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Home;
