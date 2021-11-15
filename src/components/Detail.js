5ad340b33e6ad9bc45a6666bdd2de0db0fc01a05
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-15 19:47:58 +0300

    Create folder structure

---
+++ src/components/Detail.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Hello from Detail</div>;
+};
+
+export default Home;
---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Hello from home</div>;
+};
+
+export default Home;
