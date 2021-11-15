b64427bca2db3144de9ebd9bb8c2db12f2171ac1
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-15 20:10:42 +0300

    Create I ntial header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
+import { BsChevronLeft, BsFillGearFill } from 'react-icons/bs';
+import { FaMicrophone } from 'react-icons/fa';
+
 const Header = () => (
   <header>
+    <BsChevronLeft />
     <span>most views</span>
+    <span>
+      <FaMicrophone />
+      <BsFillGearFill />
+    </span>
   </header>
 );
 
