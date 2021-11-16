0a804bb735d1482cb821d5b01eb50b7ed33c4e16
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-16 18:41:38 +0300

    Hide left chevron icon in home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import { BsChevronLeft, BsFillGearFill } from 'react-icons/bs';
 import { FaMicrophone } from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import './Header.css';
 
-const Header = () => (
-  <header>
-    <NavLink to="/" className="left-arrow-icon">
-      <BsChevronLeft className="mx-1" />
-    </NavLink>
-    <span>the covid dataset</span>
-    <span>
-      <FaMicrophone className="me-3" />
-      <BsFillGearFill className="mx-1" />
-    </span>
-  </header>
-);
+const Header = () => {
+  const location = useLocation();
+
+  return (
+    <header>
+      {location.pathname === '/detail' ? (
+        <NavLink to="/" className="left-arrow-icon">
+          <BsChevronLeft className="mx-1" />
+        </NavLink>
+      ) : (
+        <NavLink to="/" className="left-arrow-icon hidden-icon">
+          <BsChevronLeft className="mx-1" />
+        </NavLink>
+      )}
+      <span>the covid dataset</span>
+      <span>
+        <FaMicrophone className="me-3" />
+        <BsFillGearFill className="mx-1" />
+      </span>
+    </header>
+  );
+};
 
 export default Header;
